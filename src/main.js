1001a49543d479ac2ed1a1306189cfd07efbf4a8
guang.li <liguang@ecmoho.com>
2020-04-21 10:20:29 +0800

    commit first

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
 
+import "ant-design-vue/dist/antd.css";
+// import Button from "ant-design-vue/lib/button";
+// import antd from '../node_modules/ant-design-vue';
+import ant from '../node_modules/ant-design-vue';
+import App from './App.vue'
+// console.log(antd)
+Vue.use(ant);
 Vue.config.productionTip = false
 
 new Vue({

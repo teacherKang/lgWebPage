7fa778e4a8fe3b56e8a4b0ad7d878a16bf507c04
guang.li <liguang@ecmoho.com>
2020-04-21 10:21:02 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        modifyVars: {
+          "primary-color":"#1a5a5a",
+        },
+        javascriptEnabled:true,
+      }
+    }
+  }
+}
